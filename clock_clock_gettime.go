2c31bd76e7bba3b373f1b7f02d3504a3e97c1d4f
Paul Querna <pquerna@apache.org>
2016-11-23 19:06:37 -0800

    Fix clock_gettime on FreeBSD/NetBSD by using the correct value for clock_monotonic

--- clock_clock_gettime.go
+++ clock_clock_gettime.go
@@ -8,16 +8,12 @@ import (
 	"unsafe"
 )
 
-const (
-	clock_monotonic = 1
-)
-
-func monotime() int64 {
+func monotime() uint64 {
 	var ts syscall.Timespec
 
 	syscall.Syscall(syscall.SYS_CLOCK_GETTIME, clock_monotonic, uintptr(unsafe.Pointer(&ts)), 0)
 
-	return ts.Nano()
+	return uint64(ts.Nano())
 }
 
 func duration(start uint64, end uint64) time.Duration {
